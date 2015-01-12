c0f647a69bf7d07f2599a9143462f950a1a083e1
Jim Veneskey <github@strifecrafter.com>
2015-01-12 12:56:30 -0500

    Initial Commit

---
+++ Man's Best Friend/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Man's Best Friend
+//
+//  Created by jim Veneskey on 1/12/15.
+//  Copyright (c) 2015 Jim Veneskey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Man's Best Friend/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Man's Best Friend
+//
+//  Created by jim Veneskey on 1/12/15.
+//  Copyright (c) 2015 Jim Veneskey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
