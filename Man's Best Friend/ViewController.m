e1a4cbe8ece24ed283b7083f039854e8c3766c93
Jim Veneskey <github@strifecrafter.com>
2015-01-13 13:09:13 -0500

    Initial Commit

--- Man's Best Friend/ViewController.m
+++ Man's Best Friend/ViewController.m
@@ -19,7 +19,15 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    MBFDog
+    MBFDog *myDog = [[MBFDog alloc] init];
+    myDog.name = @"Lassie";
+    myDog.breed = @"Collie";
+    myDog.age = 5;
+    NSLog(@"My dog is named %@ and it's age is %i, its' breed is %@", myDog.name, myDog.age, myDog.breed);
+    myDog = nil;
+    NSLog(@"My dog is named %@ and it's age is %i, its' breed is %@", myDog.name, myDog.age, myDog.breed);
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
