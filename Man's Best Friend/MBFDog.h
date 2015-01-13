8808a49ea1a0e9e9de863a1758da2c297eff2616
Jim Veneskey <github@strifecrafter.com>
2015-01-13 13:09:06 -0500

    Initial Commit

---
+++ Man's Best Friend/MBFDog.h
@@ -0,0 +1,18 @@
+//
+//  MBFDog.h
+//  Man's Best Friend
+//
+//  Created by jim Veneskey on 1/13/15.
+//  Copyright (c) 2015 Jim Veneskey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MBFDog : NSObject
+
+@property (nonatomic) int age;
+@property (nonatomic, strong) NSString *breed;
+@property (nonatomic, strong) UIImage *image;
+
+
+@end
---
+++ Man's Best Friend/MBFDog.m
@@ -0,0 +1,13 @@
+//
+//  MBFDog.m
+//  Man's Best Friend
+//
+//  Created by jim Veneskey on 1/13/15.
+//  Copyright (c) 2015 Jim Veneskey. All rights reserved.
+//
+
+#import "MBFDog.h"
+
+@implementation MBFDog
+
+@end
